2b37764ef9ca7de21de80cc7956b8b5acc9a6399
LuisFAires <luisfillipe.lipe@gmail.com>
2026-06-02 21:33:57 -0300

    delay not needed

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const { connect } = require("puppeteer-real-browser");
 const SAME_COUNTRY = false; //true for same country, false for random
-const MESSAGE_DELAY = 50; //delay in ms between each character typed
 const MESSAGE = 'Search for "triggertaps.top" on google'; //message to send to stranger
 const INTERESTS = "" //comma separated list of interests, leave blank for random
 
@@ -100,22 +99,6 @@ async function main() {
             }, SAME_COUNTRY);
         }
 
-        async function sendMessage(message) {
-            for (character of message) {
-                await page.type('input[id="message-input"]', character)
-                await new Promise((resolve) => setTimeout(resolve, MESSAGE_DELAY));
-                let online = await isStrangerOnline();
-                if (!online) {
-                    console.log("🔴⌨️ Stranger disconnected while typing...");
-                    return false;
-                }
-            }
-            await page.click('button[id="send-btn"]');
-            console.log("✅ Sent messages to stranger ✅ ");
-            await new Promise((resolve) => setTimeout(resolve, 200));
-            return true;
-        }
-
         try {
             console.log('🕒', new Date().toLocaleTimeString());
 
@@ -145,7 +128,10 @@ async function main() {
                 return;
             }
             console.log("🟢 Stranger is online...");
-            let sendMessageResult = await sendMessage(MESSAGE);
+            await page.type('input[id="message-input"]', MESSAGE);
+            await page.click('button[id="send-btn"]');
+            console.log("✅ Sent messages to stranger ✅ ");
+            await new Promise((resolve) => setTimeout(resolve, 200));
             isOnlineNow = await isStrangerOnline();
             if (isOnlineNow) {
                 console.log("🟢 Stranger is still online, ending chat...");
